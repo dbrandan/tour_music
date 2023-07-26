43694d3ab1a37e25899e6ddf57517788c6c34bfe
Robertoczo <robertocardo49@gmail.com>
2023-07-26 13:10:59 -0300

    json datos

--- models/review.py
+++ models/review.py
@@ -9,16 +9,33 @@ class Review:
         self.reseñas=[]
     def agregar_reseña(self, reseña):
         self.reseñas.append(reseña)
-        
-    def to_json(self):
-        return {"id":self.id, "id_evento":{self.id_evento}, "id_usuario":{self.id_usuario}, "calificacion":{self.calificacion},"comentarios":{self.comentarios}, "animo":{self.animo}}
     @classmethod
-    def from_json(cls,json_data):
-        data= json.loads(json_data)
-        id = data["id"]
-        id_evento =data["id_evento"]
-        id_usuario =data["id_usuario"]
-        calificacion = data["calificacion"]
-        comentarios= data["comentarios"]
-        animo=data["animo"]
-        return cls(id, id_evento, id_usuario, calificacion, comentarios,animo) 
+    def cargar_de_json(cls, archivo):
+        with open(archivo, "r") as f:
+            data = json.load(f)
+        return [cls(**review) for review in data]
+     
+   
+   
+   
+   
+   
+   
+
+   
+   
+   
+   
+   
+   ## def to_json(self):
+  ##      return {"id":self.id, "id_evento":{self.id_evento}, "id_usuario":{self.id_usuario}, "calificacion":{self.calificacion},"comentarios":{self.comentarios}, "animo":{self.animo}}
+   ## @classmethod
+  #  def from_json(cls,json_data):
+  #      data= json.loads(json_data)
+   #     id = data["id"]
+   #     id_evento =data["id_evento"]
+   #     id_usuario =data["id_usuario"]
+   #     calificacion = data["calificacion"]
+   #     comentarios= data["comentarios"]
+   #     animo=data["animo"]
+   #     return cls(id, id_evento, id_usuario, calificacion, comentarios,animo) 
--- models/rutaVisita.py
+++ models/rutaVisita.py
@@ -4,14 +4,15 @@ class rutaVisita:
         self.id=id
         self.nombre=nombre
         self.destinos=destinos
+
    
-    def to_json(self):
-        return {"id":self.id, "nombre":{self.nombre}, "destinos":{self.destinos}}
+    ##def to_json(self):
+     return {"id":self.id, "nombre":{self.nombre}, "destinos":{self.destinos}}
     @classmethod
     def from_json(cls,json_data):
         data= json.loads(json_data)
         id = data["id"]
         nombre =data["nombre"]
         destinos =data["destinos"]
-        return cls(id, nombre, destinos,)  
+        return cls(id, nombre, destinos,)###
     
